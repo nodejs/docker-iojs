d91960428d7a030cb8b7158deeecc48e8e6b984a
Tianon Gravi <admwiggin@gmail.com>
2014-08-12 11:00:29 -0600

    Update generate-stackbrew-library.sh to handle multiple versions (and prep for adding -onbuild variations)

--- generate-stackbrew-library.sh
+++ generate-stackbrew-library.sh
@@ -1,23 +1,38 @@
 #!/bin/bash
 set -e
 
+declare -A aliases
+aliases=(
+	[0.10]='0 latest'
+)
+
 cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
 
+versions=( */ )
+versions=( "${versions[@]%/}" )
 url='git://github.com/docker-library/node'
 
 echo '# maintainer: InfoSiftr <github@infosiftr.com> (@infosiftr)'
 
-commit="$(git log -1 --format='format:%H' .)"
-fullVersion="$(grep -m1 'ENV NODE_VERSION ' Dockerfile | cut -d' ' -f3)"
-
-versionAliases=()
-while [ "${fullVersion%.*}" != "$fullVersion" ]; do
-	versionAliases+=( $fullVersion )
-	fullVersion="${fullVersion%.*}"
-done
-versionAliases+=( $fullVersion latest )
-
-echo
-for va in "${versionAliases[@]}"; do
-	echo "$va: ${url}@${commit}"
+for version in "${versions[@]}"; do
+	commit="$(git log -1 --format='format:%H' "$version")"
+	fullVersion="$(grep -m1 'ENV NODE_VERSION ' "$version/Dockerfile" | cut -d' ' -f3)"
+	versionAliases=( $fullVersion $version ${aliases[$version]} )
+	
+	echo
+	for va in "${versionAliases[@]}"; do
+		echo "$va: ${url}@${commit} $version"
+	done
+	
+#	for variant in onbuild; do
+#		echo
+#		for va in "${versionAliases[@]}"; do
+#			if [ "$va" = 'latest' ]; then
+#				va="$variant"
+#			else
+#				va="$va-$variant"
+#			fi
+#			echo "$va: ${url}@${commit} $version/$variant"
+#		done
+#	done
 done
