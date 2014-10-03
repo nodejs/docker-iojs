495f43c34ba06ed9fffa1ac59a763ac450b52ee1
Tianon Gravi <admwiggin@gmail.com>
2014-10-03 15:03:19 -0600

    Add "NPM_VERSION" updating to update.sh and bump npm version (adding it to 0.8, since 2.1.2 now supports 0.8 solidly)

--- update.sh
+++ update.sh
@@ -9,11 +9,15 @@ if [ ${#versions[@]} -eq 0 ]; then
 fi
 versions=( "${versions[@]%/}" )
 
+npmVersion="$(docker run --rm node npm show npm version)"
 for version in "${versions[@]}"; do
 	fullVersion="$(curl -sSL --compressed 'http://nodejs.org/dist' | grep '<a href="v'"$version." | sed -r 's!.*<a href="v([^"/]+)/?".*!\1!' | sort -V | tail -1)"
 	(
 		set -x
-		sed -ri 's/^(ENV NODE_VERSION) .*/\1 '"$fullVersion"'/' "$version/Dockerfile"
+		sed -ri '
+			s/^(ENV NODE_VERSION) .*/\1 '"$fullVersion"'/;
+			s/^(ENV NPM_VERSION) .*/\1 '"$npmVersion"'/;
+		' "$version/Dockerfile"
 		sed -ri 's/^(FROM node):.*/\1:'"$fullVersion"'/' "$version/onbuild/Dockerfile"
 	)
 done
