ddbb6a963d6bbee15eee8ff95e6fb604d8bff110
Peter Petrov <onestone@gmail.com>
2015-01-15 03:22:02 +0200

    Change repo name, and adapt update.sh

--- update.sh
+++ update.sh
@@ -9,15 +9,15 @@ if [ ${#versions[@]} -eq 0 ]; then
 fi
 versions=( "${versions[@]%/}" )
 
-npmVersion="$(docker run --rm node npm show npm version)"
+npmVersion="$(docker run --rm onestone/iojs npm show npm version)"
 for version in "${versions[@]}"; do
-	fullVersion="$(curl -sSL --compressed 'http://nodejs.org/dist' | grep '<a href="v'"$version." | sed -r 's!.*<a href="v([^"/]+)/?".*!\1!' | sort -V | tail -1)"
+	fullVersion="$(curl -sSL --compressed 'https://iojs.org/dist' | grep '<a href="v'"$version." | sed -r 's!.*<a href="v([^"/]+)/?".*!\1!' | sort -V | tail -1)"
 	(
 		set -x
 		sed -ri '
-			s/^(ENV NODE_VERSION) .*/\1 '"$fullVersion"'/;
+			s/^(ENV IOJS_VERSION) .*/\1 '"$fullVersion"'/;
 			s/^(ENV NPM_VERSION) .*/\1 '"$npmVersion"'/;
 		' "$version/Dockerfile" "$version/slim/Dockerfile"
-		sed -ri 's/^(FROM node):.*/\1:'"$fullVersion"'/' "$version/onbuild/Dockerfile"
+		sed -ri 's/^(FROM onestone/iojs):.*/\1:'"$fullVersion"'/' "$version/onbuild/Dockerfile"
 	)
 done
